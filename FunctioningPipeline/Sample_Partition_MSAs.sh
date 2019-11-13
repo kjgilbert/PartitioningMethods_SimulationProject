854e81b7c9efc540d7435f45959c0e002007bf27
kjgilbert <kgilbert@zoology.ubc.ca>
2019-11-13 15:55:46 +0100

    deal with jupyter kernel isues between python 2 and 3, now works and can import Bio to run the conversion from .fa to .phy for partition finder2

---
+++ FunctioningPipeline/Sample_Partition_MSAs.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+### take each MSA file and sample the first 20, 40, 100, or all character columns
+### rename and put into a new directory for the next step (partitioning and treebuilding)
+
+inDirLopho="results_lopho/sim_match_lophotroch/MSA/"
+inDirMyria="results_myria/sim_match_myriapod/MSA/"
+outDir="sampledMSAs/"
+msaName1="MSA_"
+msaName2="_aa.fa"
+
+## mkdir $outDir
+
+for i in `seq 1 200`;
+    do
+    	
+        inFileNameLo=${inDirLopho}${msaName1}$i${msaName2}
+        inFileNameMy=${inDirMyria}${msaName1}$i${msaName2}
+		
+		# do the sampling and the subsequent partitioning (which requires Phylip format)
+		
+		# N=20
+		outLo="_aa_lopho_N20.fa"
+		outMy="_aa_myria_N20.fa"
+        outFileNameLo=${outDir}${msaName1}$i${outLo}
+        outFileNameMy=${outDir}${msaName1}$i${outMy}
+		cut -c-20 $inFileNameLo > $outFileNameLo
+		cut -c-20 $inFileNameMy > $outFileNameMy
+
+		# change to phylip format
+		
+		# paritition		
+		python ../../partition_finder_2/partitionfinder-2.1.1/PartitionFinderProtein.py 
+
+
+		# N=40
+		outLo="_aa_lopho_N40.fa"
+		outMy="_aa_myria_N40.fa"
+        outFileNameLo=${outDir}${msaName1}$i${outLo}
+        outFileNameMy=${outDir}${msaName1}$i${outMy}
+		cut -c-40 $inFileNameLo > $outFileNameLo
+		cut -c-40 $inFileNameMy > $outFileNameMy
+
+		# N=100
+		outLo="_aa_lopho_N100.fa"
+		outMy="_aa_myria_N100.fa"
+        outFileNameLo=${outDir}${msaName1}$i${outLo}
+        outFileNameMy=${outDir}${msaName1}$i${outMy}
+		cut -c-100 $inFileNameLo > $outFileNameLo
+		cut -c-100 $inFileNameMy > $outFileNameMy
+
+		# N = all
+		outLo="_aa_lopho_Nall.fa"
+		outMy="_aa_myria_Nall.fa"
+        outFileNameLo=${outDir}${msaName1}$i${outLo}
+        outFileNameMy=${outDir}${msaName1}$i${outMy}
+		cat $inFileNameLo > $outFileNameLo
+		cat $inFileNameMy > $outFileNameMy
+
+done

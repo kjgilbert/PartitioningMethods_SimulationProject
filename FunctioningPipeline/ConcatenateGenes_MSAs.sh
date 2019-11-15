9db3bfbcec37b3e22b7175bf2b6de386746428fa
kjgilbert <kgilbert@zoology.ubc.ca>
2019-11-15 13:08:47 +0100

    working up to and including partitionfinder in an automated fashion, woohoo, now need to sort out using partition finder output for tree building

--- FunctioningPipeline/ConcatenateGenes_MSAs.sh
+++ FunctioningPipeline/ConcatenateGenes_MSAs.sh
@@ -8,11 +8,14 @@ myria_dir='results_myria/sim_match_myriapod/MSA/'
 ## concatenate 10 genes
 
 STR=''
-STR2=''
+##STR2=''
+rm sampledMSAs/config_lopho_10genes.txt; touch sampledMSAs/config_lopho_10genes.txt
 for i in `seq 1 10`;
 	do
-    STR=$STR${lopho_dir}'MSA_'$i'_aa.fa '
-    STR2=$STR2'$'$i' '
+	file=${lopho_dir}'MSA_'$i'_aa.fa '
+    STR=$STR$file
+##    STR2=$STR2'$'$i' '
+    head -n 2 $file | tail -n 1 | wc -m >> sampledMSAs/config_lopho_10genes.txt
 done
 ##echo $STR 
 ##echo $STR2 
@@ -21,10 +24,12 @@ paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10} else {pri
 ##paste -d '\0' $STR | sed 's/>[A-Z]*//' > sampledMSAs/MSA_lopho_10genes.fa
  
 STR=''
+rm sampledMSAs/config_myria_10genes.txt; touch sampledMSAs/config_myria_10genes.txt
 for i in `seq 1 10`;
 	do
-    STR=$STR${myria_dir}'MSA_'$i'_aa.fa '
-    STR2=$STR2'$'$i' '
+	file=${myria_dir}'MSA_'$i'_aa.fa '
+    STR=$STR$file
+    head -n 2 $file | tail -n 1 | wc -m >> sampledMSAs/config_myria_10genes.txt
 done
 paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10} else {print $1}}' > sampledMSAs/MSA_myria_10genes.fa
  
@@ -34,19 +39,23 @@ paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10} else {pri
 ## concatenate 20 genes
 
 STR=''
+rm sampledMSAs/config_lopho_20genes.txt; touch sampledMSAs/config_lopho_20genes.txt
 for i in `seq 1 20`;
 	do
-    STR=$STR${lopho_dir}'MSA_'$i'_aa.fa '
-    STR2=$STR2'$'$i' '
+	file=${lopho_dir}'MSA_'$i'_aa.fa '
+    STR=$STR$file
+    head -n 2 $file | tail -n 1 | wc -m >> sampledMSAs/config_lopho_20genes.txt
 done
 paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16 $17 $18 $19 $20} else {print $1}}' > sampledMSAs/MSA_lopho_20genes.fa
  
  
 STR=''
+rm sampledMSAs/config_myria_20genes.txt; touch sampledMSAs/config_myria_20genes.txt
 for i in `seq 1 20`;
 	do
-    STR=$STR${myria_dir}'MSA_'$i'_aa.fa '
-    STR2=$STR2'$'$i' '
+	file=${myria_dir}'MSA_'$i'_aa.fa '
+    STR=$STR$file
+    head -n 2 $file | tail -n 1 | wc -m >> sampledMSAs/config_myria_20genes.txt
 done
 paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16 $17 $18 $19 $20} else {print $1}}' > sampledMSAs/MSA_myria_20genes.fa
  
@@ -58,19 +67,23 @@ paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $
 
 
 STR=''
+rm sampledMSAs/config_lopho_40genes.txt; touch sampledMSAs/config_lopho_40genes.txt
 for i in `seq 1 40`;
 	do
-    STR=$STR${lopho_dir}'MSA_'$i'_aa.fa '
-    STR2=$STR2'$'$i' '
+	file=${lopho_dir}'MSA_'$i'_aa.fa '
+    STR=$STR$file
+    head -n 2 $file | tail -n 1 | wc -m >> sampledMSAs/config_lopho_40genes.txt
 done
 paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16 $17 $18 $19 $20 $21 $22 $23 $24 $25 $26 $27 $28 $29 $30 $31 $32 $33 $34 $35 $36 $37 $38 $39 $40} else {print $1}}' > sampledMSAs/MSA_lopho_40genes.fa
  
  
 STR=''
+rm sampledMSAs/config_myria_40genes.txt; touch sampledMSAs/config_myria_40genes.txt
 for i in `seq 1 40`;
 	do
-    STR=$STR${myria_dir}'MSA_'$i'_aa.fa '
-    STR2=$STR2'$'$i' '
+	file=${myria_dir}'MSA_'$i'_aa.fa '
+    STR=$STR$file
+    head -n 2 $file | tail -n 1 | wc -m >> sampledMSAs/config_myria_40genes.txt
 done
 paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16 $17 $18 $19 $20 $21 $22 $23 $24 $25 $26 $27 $28 $29 $30 $31 $32 $33 $34 $35 $36 $37 $38 $39 $40} else {print $1}}' > sampledMSAs/MSA_myria_40genes.fa
 
@@ -81,18 +94,22 @@ paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $
 
 
 STR=''
+rm sampledMSAs/config_lopho_80genes.txt; touch sampledMSAs/config_lopho_80genes.txt
 for i in `seq 1 80`;
 	do
-    STR=$STR${lopho_dir}'MSA_'$i'_aa.fa '
-    STR2=$STR2'$'$i' '
+	file=${lopho_dir}'MSA_'$i'_aa.fa '
+    STR=$STR$file
+    head -n 2 $file | tail -n 1 | wc -m >> sampledMSAs/config_lopho_80genes.txt
 done
 paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16 $17 $18 $19 $20 $21 $22 $23 $24 $25 $26 $27 $28 $29 $30 $31 $32 $33 $34 $35 $36 $37 $38 $39 $40 $41 $42 $43 $44 $45 $46 $47 $48 $49 $50 $51 $52 $53 $54 $55 $56 $57 $58 $59 $60 $61 $62 $63 $64 $65 $66 $67 $68 $69 $70 $71 $72 $73 $74 $75 $76 $77 $78 $79 $80} else {print $1}}' > sampledMSAs/MSA_lopho_80genes.fa
  
  
 STR=''
+rm sampledMSAs/config_myria_80genes.txt; touch sampledMSAs/config_myria_80genes.txt
 for i in `seq 1 80`;
 	do
-    STR=$STR${myria_dir}'MSA_'$i'_aa.fa '
-    STR2=$STR2'$'$i' '
+	file=${myria_dir}'MSA_'$i'_aa.fa '
+    STR=$STR$file
+    head -n 2 $file | tail -n 1 | wc -m >> sampledMSAs/config_myria_80genes.txt
 done
 paste $STR | awk '{if (NR%2==0) {print $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16 $17 $18 $19 $20 $21 $22 $23 $24 $25 $26 $27 $28 $29 $30 $31 $32 $33 $34 $35 $36 $37 $38 $39 $40 $41 $42 $43 $44 $45 $46 $47 $48 $49 $50 $51 $52 $53 $54 $55 $56 $57 $58 $59 $60 $61 $62 $63 $64 $65 $66 $67 $68 $69 $70 $71 $72 $73 $74 $75 $76 $77 $78 $79 $80} else {print $1}}' > sampledMSAs/MSA_myria_80genes.fa
--- FunctioningPipeline/PartitionMSAs.sh
+++ FunctioningPipeline/PartitionMSAs.sh
@@ -11,6 +11,105 @@ files=${input_folder}*.phy
 
 # put the cfg file in each corresponding directory
 
+aic_string=$'## ALIGNMENT FILE ##
+alignment = temp_align.phy;
+
+## BRANCHLENGTHS: linked | unlinked ##
+branchlengths = linked;
+
+## MODELS OF EVOLUTION: all | allx | mrbayes | beast | gamma | gammai | <list> ##
+models = all;    # allx uses ML instead of empirical estimation of amino acid frequencies
+
+# MODEL SELECCTION: AIC | AICc | BIC #
+model_selection = aic;
+
+## DATA BLOCKS: see manual for how to define ##
+[data_blocks]
+'
+
+aicc_string=$'## ALIGNMENT FILE ##
+alignment = temp_align.phy;
+
+## BRANCHLENGTHS: linked | unlinked ##
+branchlengths = linked;
+
+## MODELS OF EVOLUTION: all | allx | mrbayes | beast | gamma | gammai | <list> ##
+models = all;    # allx uses ML instead of empirical estimation of amino acid frequencies
+
+# MODEL SELECCTION: AIC | AICc | BIC #
+model_selection = aicc;
+
+## DATA BLOCKS: see manual for how to define ##
+[data_blocks]
+'
+
+bic_string=$'## ALIGNMENT FILE ##
+alignment = temp_align.phy;
+
+## BRANCHLENGTHS: linked | unlinked ##
+branchlengths = linked;
+
+## MODELS OF EVOLUTION: all | allx | mrbayes | beast | gamma | gammai | <list> ##
+models = all;    # allx uses ML instead of empirical estimation of amino acid frequencies
+
+# MODEL SELECCTION: AIC | AICc | BIC #
+model_selection = bic;
+
+## DATA BLOCKS: see manual for how to define ##
+[data_blocks]
+'
+
+rcluster_string=$'
+## SCHEMES, search: all | user | greedy | rcluster | rclusterf | kmeans ##
+[schemes]
+search = rcluster;  # only works with raxml command line specification
+'
+
+greedy_string=$'
+## SCHEMES, search: all | user | greedy | rcluster | rclusterf | kmeans ##
+[schemes]
+search = greedy;
+'
+
+
+
+# do all N10
+
+# aic rcluster
+for f in files;
+	do
+	cp $f part_N10/temp_align.phy
+	# make the partition file based on the gene sites stored in config_genes__.txt
+		# format the gene sites
+		# species:
+		species=$(echo $f | tr "_" "\n" | tail -n 2 | head -n 1)
+		# sample size of genes
+		sample_size=$(echo $f | tr "_" "\n" | tail -n 1 | tr "." "\n" | head -n 1)
+		gene_sites_file=$input_folder'config_'$species'_'$sample_size'.txt'
+		gene_string=''
+		gene_end='0'
+		prev_gene_end='0'
+		it='0'
+		while read line
+		    do
+		    it=$(expr $it + 1)
+		    gene_start=$(expr $gene_end + 1)
+		    gene_end=$(expr $line - 1 + $prev_gene_end)
+		    prev_gene_end=$(expr $line - 1 + $prev_gene_end)
+		    gene_string=$gene_string'gene'$it' = '$gene_start'-'$gene_end$';\n'
+#			echo $gene_start
+#			echo $gene_end
+#			echo $prev_gene_end
+		done <$gene_sites_file
+		
+		# put all the strings together into the final .cfg file
+		echo "$bic_string" "$gene_string" "$greedy_string" > test.txt
+
+	cp partition_finder_N10_raxml_aic.cfg part_N10/partition_finder.cfg	
+	python2.7 ../../partition_finder_2/partitionfinder-2.1.1/PartitionFinderProtein.py part_N10/ --raxml -p 4
+done
+
+
 # do all N20
 
 # aic rcluster

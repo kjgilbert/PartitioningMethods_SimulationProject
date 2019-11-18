54d78764ce02a51ed7be886bae3900c1fe75d96d
kjgilbert <kgilbert@zoology.ubc.ca>
2019-11-18 13:11:07 +0100

    robinson foulds and euclidean distance measures both working

--- FunctioningPipeline/AnalysisResults/prelimResults_analysis.R
+++ FunctioningPipeline/AnalysisResults/prelimResults_analysis.R
@@ -65,7 +65,18 @@ points(c(1,1.1,2,2.1,3,3.1,4), c(dat$iqTreeAIC[3], dat$iqTreeAIC[4], dat$iqTreeA
 ##points(c(1,1.1,2,2.1,3,3.1,4), c(dat$iqTreeAICc[3], dat$iqTreeAICc[4], dat$iqTreeAICc[2], dat$iqTreeAICc[6], dat$iqTreeAICc[1], dat$iqTreeAICc[5], dat$iqTreeAICc[7]), col="green3", lwd=2)
 points(c(1,1.1,2,2.1,3,3.1,4), c(dat$iqTreeBIC[3], dat$iqTreeBIC[4], dat$iqTreeBIC[2], dat$iqTreeBIC[6], dat$iqTreeBIC[1], dat$iqTreeBIC[5], dat$iqTreeBIC[7]), col="orange", lwd=2)
 #legend("left", c("rcluster", "greedy"), col=c("black", "red"), pch=16)
-#legend("right", c("IQTree AIC", "IQTree BIC"), col=c("blue", "orange"), pch=1, pt.lwd=2)
+legend("topright", c("IQTree BIC"), col=c("orange"), pch=1, pt.lwd=2)
 
-text(x=c(1,1.1,2,2.1,3,3.1,4), y=rep(103870,6), labels=c(dat$numParts[3:4], dat$numParts[2], dat$numParts[6], dat$numParts[1], dat$numParts[5], 0))
+text(x=c(1,1.1,2,2.1,3,3.1,4), y=rep(104557,6), labels=c(dat$numParts[3:4], dat$numParts[2], dat$numParts[6], dat$numParts[1], dat$numParts[5], 0))
 
+
+plot(c(1:4, 1.1:3.1), rep(0.0847457627118644, 7), xaxt="n", ylim=c(0.08, 0.09), xlab="Partitioning Criterion", ylab="Normalized Robinson-Foulds", pch=15)
+axis(side=1, at=1:4, labels=c("AIC", "AICc", "BIC", "none"))
+
+
+
+
+
+
+plot(c(1:4, 1.1:3.1), c(498.1729185398443, 498.17346684526217, 498.1721147408412,   498.1721147408412, 498.16853197394056, 498.1586883819632     , 498.1758378443058), xaxt="n", ylim=c(498.14,498.2), xlab="Partitioning Criterion", ylab="Euclidean Distance", pch=15)
+axis(side=1, at=1:4, labels=c("AIC", "AICc", "BIC", "none"))

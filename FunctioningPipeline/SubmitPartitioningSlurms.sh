bbce33573c457d6a3ca78111a6f4c52b844d713e
kjgilbert <kgilbert@zoology.ubc.ca>
2019-11-26 17:33:52 +0100

    clean up, should be working now but still need to test

--- FunctioningPipeline/SubmitPartitioningSlurms.sh
+++ FunctioningPipeline/SubmitPartitioningSlurms.sh
@@ -2,88 +2,43 @@
 
 
 # make all the directories
+current_path='/scratch/wally/FAC/FBM/DBC/cdessim2/default/kgilbert/Genes_Length100/'
+##__________________________________##
+##__________________ /\  ___________##
+##_________MODIFY ME ||  ___________##
+##__________________ ||  ___________##
+##__________________________________##
+
 
 run_path='/scratch/wally/FAC/FBM/DBC/cdessim2/default/kgilbert/'
-curr_path='/scratch/wally/FAC/FBM/DBC/cdessim2/default/kgilbert/Genes_Length100/'
 
 
-${curr_path}SetupAndSimulate.sh $curr_path $run_path
+${current_path}SetupAndSimulate.sh $current_path $run_path
 
-# simulations are done, input files are reformatted and put into folders, now run each set with partitionfinder and IQtree
+# simulations are done, input files are reformatted and put into folders, 
+#     now run each set with partitionfinder and IQtree
 
 
 ######### DO ALL THE PARTITIONING AND IQTREE analyses
 
+input_folder=$current_path'sampledMSAs/'
+files=${input_folder}*.phy
 
-### s = -0.001
-
-dirname=s0p001_h0p5_rep
-for i in `seq $1 $2`;
+for i in $files;
     do
-        fulldirname=${basedirname}${dirname}$i
-        sbatch "$fulldirname/Run.slurm"        
+    # sample size of genes
+    sample_size=$(echo $f | tr "_" "\n" | tail -n 1 | tr "." "\n" | head -n 1)
+    #raw numeric sample size for directory label
+    raw_samp=$(echo $sample_size | tr "g" "\n" | head -n 1)
+    # species:
+    species=$(echo $f | tr "_" "\n" | tail -n 2 | head -n 1)
+    
+    sbatch ${current_path}'part_N'$raw_samp'_'$species'_r-a/Run.slurm'
+	sbatch ${current_path}'part_N'$raw_samp'_'$species'_r-b/Run.slurm'
+	sbatch ${current_path}'part_N'$raw_samp'_'$species'_r-c/Run.slurm'
+	sbatch ${current_path}'part_N'$raw_samp'_'$species'_g-a/Run.slurm'
+	sbatch ${current_path}'part_N'$raw_samp'_'$species'_g-b/Run.slurm'
+	sbatch ${current_path}'part_N'$raw_samp'_'$species'_g-c/Run.slurm'
+
 done
 
-dirname=s0p001_h0p25_rep
-for i in `seq $1 $2`;
-    do
-        fulldirname=${basedirname}${dirname}$i
-        sbatch "$fulldirname/Run.slurm"        
-done
-
-dirname=s0p001_h0p0_rep
-for i in `seq $1 $2`;
-    do
-        fulldirname=${basedirname}${dirname}$i
-        sbatch "$fulldirname/Run.slurm"        
-done
-
-
-
-### s = -0.01
-
-dirname=s0p01_h0p5_rep
-for i in `seq $1 $2`;
-    do
-        fulldirname=${basedirname}${dirname}$i
-        sbatch "$fulldirname/Run.slurm"        
-done
-
-dirname=s0p01_h0p25_rep
-for i in `seq $1 $2`;
-    do
-        fulldirname=${basedirname}${dirname}$i
-        sbatch "$fulldirname/Run.slurm"        
-done
-
-dirname=s0p01_h0p0_rep
-for i in `seq $1 $2`;
-    do
-        fulldirname=${basedirname}${dirname}$i
-        sbatch "$fulldirname/Run.slurm"        
-done
-
-
-
-### s = -0.1
-
-dirname=s0p1_h0p5_rep
-for i in `seq $1 $2`;
-    do
-        fulldirname=${basedirname}${dirname}$i
-        sbatch "$fulldirname/Run.slurm"        
-done
-
-dirname=s0p1_h0p25_rep
-for i in `seq $1 $2`;
-    do
-        fulldirname=${basedirname}${dirname}$i
-        sbatch "$fulldirname/Run.slurm"        
-done
-
-dirname=s0p1_h0p0_rep
-for i in `seq $1 $2`;
-    do
-        fulldirname=${basedirname}${dirname}$i
-        sbatch "$fulldirname/Run.slurm"        
-done
